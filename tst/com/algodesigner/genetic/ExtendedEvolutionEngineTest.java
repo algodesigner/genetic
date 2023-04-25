7adc79c541909f6c9e2993e24a143125b97d988f
Vlad Shurupov <vlad@algotradinggroup.com>
2023-04-25 20:05:22 +1000

    Further formatting

--- tst/com/algodesigner/genetic/ExtendedEvolutionEngineTest.java
+++ tst/com/algodesigner/genetic/ExtendedEvolutionEngineTest.java
@@ -22,41 +22,42 @@ public class ExtendedEvolutionEngineTest {
   private static final TerminationCriteria TERMINATION_CRITERIA =
     new TerminationCriteria(1000 * 60 * 10, 10000);
   private static final long SEED = 314159265;
-  
+
   private static final byte[] PERFECT_SEQ = "/'|.".getBytes();
-  private static final Chromosome PERFECT_CHROMOSOME = new Chromosome("/'|./'|./'|./'|./'|./'|./'|./'|./'|./'|./'|./'|.");
+  private static final Chromosome PERFECT_CHROMOSOME =
+    new Chromosome("/'|./'|./'|./'|./'|./'|./'|./'|./'|./'|./'|./'|.");
   private static final double PERFECT_SCORE;
-  
+
   /*
    * This fitness function favours one dominating gene, be it '0' or '1'. The
    * '.' gene is considered useless.
    */
   private static final IFitnessFunction fitnessFunction = chromosome -> {
-	double score = 0;
-	byte[] g = chromosome.toString().getBytes();
-	for (int i = 0; i < g.length; i++) {
-		boolean matched = true;
-		int j = i;
-		for (; j < g.length && (j - i) < PERFECT_SEQ.length; j++) {
-			if (g[j] != PERFECT_SEQ[j - i]) {
-				matched = false;
-				break;
-			}
-		}
-		if (j - i == PERFECT_SEQ.length && matched)
-			score += 1;
-	}
-	return score;
+    double score = 0;
+    byte[] g = chromosome.toString().getBytes();
+    for (int i = 0; i < g.length; i++) {
+      boolean matched = true;
+      int j = i;
+      for (; j < g.length && (j - i) < PERFECT_SEQ.length; j++) {
+        if (g[j] != PERFECT_SEQ[j - i]) {
+          matched = false;
+          break;
+        }
+      }
+      if (j - i == PERFECT_SEQ.length && matched)
+        score += 1;
+    }
+    return score;
   };
-  
+
   static {
-	  PERFECT_SCORE = fitnessFunction.apply(PERFECT_CHROMOSOME);
-	  System.out.println("Perfect score: " + PERFECT_SCORE);
+    PERFECT_SCORE = fitnessFunction.apply(PERFECT_CHROMOSOME);
+    System.out.println("Perfect score: " + PERFECT_SCORE);
   }
 
   @Test
   public void testEvolution() {
-//    checkEvolution(true);
+    // checkEvolution(true);
     checkEvolution(false);
   }
 
@@ -64,20 +65,20 @@ public class ExtendedEvolutionEngineTest {
     // Seed the pseudorandom number generator to achieve predictable results
     Random random = new Random();
     EvolutionEngine engine = new EvolutionEngine(createInitialGeneration(),
-      CROSSOVER_RATE, MUTATION_RATE, fitnessFunction, elitism, random);    
+      CROSSOVER_RATE, MUTATION_RATE, fitnessFunction, elitism, random);
 
     System.out.println(engine);
 
     // This is an optional step to ensure this call does not break anything
-//    engine.step();
+    // engine.step();
 
-//    int bestIndex = engine.findSolution(PERFECT_SCORE, TERMINATION_CRITERIA);
-//    assertTrue(bestIndex > -1);
-    
+    // int bestIndex = engine.findSolution(PERFECT_SCORE, TERMINATION_CRITERIA);
+    // assertTrue(bestIndex > -1);
+
     System.out
       .println("Problem Solved! Generation: " + engine.getGenerationCount());
     System.out.println("Best score: " + engine.getBestFitnessScore());
-    
+
     assertTrue(engine.getGenerationCount() > 0);
     assertTrue(engine.getBestIndex() < engine.getGeneration().size());
 
@@ -103,30 +104,30 @@ public class ExtendedEvolutionEngineTest {
     // Seed the pseudorandom number generator to achieve predictable results
     Random random = new Random(SEED);
     EvolutionEngine engine = new EvolutionEngine(createInitialGeneration(),
-      CROSSOVER_RATE, MUTATION_RATE, fitnessFunction, true, random);    
+      CROSSOVER_RATE, MUTATION_RATE, fitnessFunction, true, random);
     engine.findSolution(1, null);
     assertEquals((double)1, engine.getBestFitnessScore(), 1e-8);
   }
 
   private static Generation createInitialGeneration() {
     GenerationBuilder builder = new GenerationBuilder();
-    
-	builder.addChromosome("/'|.||||||||||||||||||||||||||||||||||||||||||||");
-	builder.addChromosome("////////////////////////////////////////////////");
-	builder.addChromosome("''''''''''''''''''''''''''''''''''''''''''''''''");
-	builder.addChromosome("................................................");
-	builder.addChromosome("////////////////////////////////////////////////");
-	builder.addChromosome("||||||||||||||||||||||||||||||||||||||||||||||||");
-	builder.addChromosome("''''''''''''''''''''''''''''''''''''/'|.''''''''");
-	builder.addChromosome("................................................");
-	builder.addChromosome("////////////////////////////////////////////////");
-	builder.addChromosome("||||||||||||||||||||||||||||||||||||||||||||||||");
-	builder.addChromosome("''''''''''''''''''''''''''''''''''''''''''''''''");
-	builder.addChromosome("................................................");
-	builder.addChromosome("////////////'|./////////////////////////////////");
-	builder.addChromosome("||||||||||||||||||||||||||||||||||||||||||||||||");
-	builder.addChromosome("''''''''''''''''''''''''''''''''''''''''''''''''");
-	builder.addChromosome("................................................");
+
+    builder.addChromosome("/'|.||||||||||||||||||||||||||||||||||||||||||||");
+    builder.addChromosome("////////////////////////////////////////////////");
+    builder.addChromosome("''''''''''''''''''''''''''''''''''''''''''''''''");
+    builder.addChromosome("................................................");
+    builder.addChromosome("////////////////////////////////////////////////");
+    builder.addChromosome("||||||||||||||||||||||||||||||||||||||||||||||||");
+    builder.addChromosome("''''''''''''''''''''''''''''''''''''/'|.''''''''");
+    builder.addChromosome("................................................");
+    builder.addChromosome("////////////////////////////////////////////////");
+    builder.addChromosome("||||||||||||||||||||||||||||||||||||||||||||||||");
+    builder.addChromosome("''''''''''''''''''''''''''''''''''''''''''''''''");
+    builder.addChromosome("................................................");
+    builder.addChromosome("////////////'|./////////////////////////////////");
+    builder.addChromosome("||||||||||||||||||||||||||||||||||||||||||||||||");
+    builder.addChromosome("''''''''''''''''''''''''''''''''''''''''''''''''");
+    builder.addChromosome("................................................");
 
     return builder.build();
   }
